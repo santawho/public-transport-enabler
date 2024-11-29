fdc88e4cdbd3cde04afa7bdfd7fb20b20d168ed0
Jonah Brüchert <jbb@kaidan.im>
2024-11-29 14:35:15 +0100

    Split motis and transitous provider

---
+++ src/de/schildbach/pte/TransitousProvider.java
@@ -0,0 +1,7 @@
+package de.schildbach.pte;
+
+public class TransitousProvider extends AbstractMOTISProvider {
+    public TransitousProvider() {
+        super("https://europe.motis-project.de/");
+    }
+}
