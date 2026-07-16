ccc2fa3e1295c242baa376ba1839f39590f7cb6a
santawho <196087559+santawho@users.noreply.github.com>
2026-07-16 14:58:01 +0200

    VAO: split directions like station names

--- src/de/schildbach/pte/provider/hafas/austria/VaoProvider.java
+++ src/de/schildbach/pte/provider/hafas/austria/VaoProvider.java
@@ -93,6 +93,11 @@ public class VaoProvider extends AbstractHafasClientInterfaceProvider {
     };
 
     @Override
+    protected String[] splitDirectionName(final String placeAndName) {
+        return parseSpaceDelimitedPlaceAndStation(placeAndName, SPECIAL_PLACES);
+    }
+
+    @Override
     protected String[] splitStationName(final String placeAndName) {
         return parseSpaceDelimitedPlaceAndStation(placeAndName, SPECIAL_PLACES);
     }
