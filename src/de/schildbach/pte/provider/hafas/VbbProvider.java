1a9f0781ead876104c6048f57eeadc8a88afd83b
santawho <196087559+santawho@users.noreply.github.com>
2026-09-04 12:53:02 +0200

    VBB provider: expect destinations as directions on station board

--- src/de/schildbach/pte/provider/hafas/VbbProvider.java
+++ src/de/schildbach/pte/provider/hafas/VbbProvider.java
@@ -17,15 +17,12 @@
 
 package de.schildbach.pte.provider.hafas;
 
-import androidx.annotation.Nullable;
-
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import de.schildbach.pte.NetworkId;
 import de.schildbach.pte.dto.Fare;
-import de.schildbach.pte.dto.Line;
 import de.schildbach.pte.dto.Product;
 
 import okhttp3.HttpUrl;
