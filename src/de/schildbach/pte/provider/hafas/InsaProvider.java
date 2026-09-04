bf9449fc3319f845a55a20c1d5a9b0f29af928da
santawho <196087559+santawho@users.noreply.github.com>
2026-09-04 12:53:31 +0200

    INSA, NASA, MDV providers: expect destinations as directions on station board

--- src/de/schildbach/pte/provider/hafas/InsaProvider.java
+++ src/de/schildbach/pte/provider/hafas/InsaProvider.java
@@ -104,6 +104,11 @@ public abstract class InsaProvider extends AbstractHafasClientInterfaceProvider 
     }
 
     @Override
+    protected boolean isStationBoardDestinationCommonlyDirection() {
+        return false;
+    }
+
+    @Override
     protected String[] splitStationName(final String name) {
         final Matcher m = P_SPLIT_NAME_FIRST_COMMA.matcher(name);
         if (m.matches())
